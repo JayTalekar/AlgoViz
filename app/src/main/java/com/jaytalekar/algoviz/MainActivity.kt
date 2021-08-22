0d368bceddd31f258522441dfdf2de691c0afbd2
Jay Talekar <jaytalekar.developer@gmail.com>
2021-08-22 21:47:48 +0530

    Initial commit

---
+++ app/src/main/java/com/jaytalekar/algoviz/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jaytalekar.algoviz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jaytalekar.algoviz.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
